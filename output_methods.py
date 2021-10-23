4192c96b54a57050df644bfcbe26f2d1d1b7d588
Jakub Maloštík <xmalost@fi.muni.cz>
2021-10-23 20:14:46 +0200

    add output methods: csv, sample-output

---
+++ output_methods.py
@@ -0,0 +1,65 @@
+from sys import stdout, stderr
+
+output_methods: dict = {}
+
+
+def output_method_class(argument_name):
+    def decorator(c):
+        if argument_name in output_methods:
+            raise ValueError(
+                f"Output method argument name {argument_name} already exists")
+        output_methods[argument_name] = c
+        return c
+    return decorator
+
+
+@output_method_class('sample-output')
+class SampleOutput:
+    default_parameters = {
+        "output_file": stdout,
+        "err_file": stderr,
+        "entropy_threshold": 1.0
+    }
+
+    def __init__(self, **kwargs):
+        for key, value in (SampleOutput.default_parameters | kwargs).items():
+            if key in SampleOutput.default_parameters:
+                setattr(self, key, value)
+
+    def output(self,
+               sector_number,
+               sector_offset,
+               sector_entropy,
+               sector_pattern):
+        if self.entropy_threshold > sector_entropy:
+            print(
+                f"{sector_number} (0x{sector_offset:x}) - {sector_entropy:.4f}" +
+                (f" (pattern of 0x{sector_pattern:02x})" if sector_pattern is not None else ""),
+                file=self.output_file)
+
+    def error(self, message):
+        print(message, file=self.err_file)
+
+
+@output_method_class('csv')
+class CSVOutput:
+    default_parameters = {
+        "output_file": stdout,
+        "err_file": stderr,
+        "entropy_threshold": 1.0
+    }
+
+    def __init__(self, **kwargs):
+        for key, value in (SampleOutput.default_parameters | kwargs).items():
+            if key in SampleOutput.default_parameters:
+                setattr(self, key, value)
+        print("SECTOR_NUM,SECTOR_OFFSET,SECTOR_ENTROPY,PATTERN")
+
+    def output(self, *args):
+        if self.entropy_threshold > args[2]:
+            print(
+                ','.join('' if x is None else str(x) for x in args),
+                file=self.output_file)
+
+    def error(self, message):
+        print(message, file=self.err_file)
